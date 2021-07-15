dbae7cbda7798f477fb4b333fb5eb8ac671bec74
unknown <subedipradeep14@gmail.com>
2021-07-15 22:14:01 +0545

    first commit

---
+++ lib/providers/product.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Product with ChangeNotifier {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
+    final oldStatus = isFavorite;
+    isFavorite = !isFavorite;
+    notifyListeners();
+    final url =
+        'https://my-shop-1bac0-default-rtdb.firebaseio.com/userFavorites/$userId/$id.json?auth=$token';
+    // use your own url
+    try {
+      final response = await http.put(
+        Uri.parse(url),
+        body: json.encode(
+          isFavorite,
+        ),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+        print('Hellooooooooooooooo');
+        print(userId);
+        print(token);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
+  }
+}
