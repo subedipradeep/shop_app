a8b037e2aa98f77c03b4c1bef9d3ddf1ac9ca53b
subedipradeep <47079575+subedipradeep@users.noreply.github.com>
2021-08-14 09:39:29 +0545

    Update auth.dart

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -33,7 +33,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyA6UZg8VZpZ9T4c5LRjXqN8mAsR_6J3hBE'; // use your own url
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=YOUR-KEY'; // use your own url
     try {
       final response = await http.post(
         Uri.parse(url),
